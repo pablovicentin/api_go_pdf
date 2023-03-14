cbc8f768dd5a310f89d542b09e1457fd6d73f7b5
pablovicentin <pablo.vicentin@telco.com.ar>
2023-03-14 08:49:53 -0300

    reversiones

--- sample1.go
+++ sample1.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/johnfercher/maroto/pkg/color"
@@ -45,9 +47,17 @@ type reversion struct {
 	m pdf.Maroto
 }
 
-type HeaderReversionData struct {
+type ReversionData struct {
 	Pago    PagoData
 	Intento IntentoData
+	Items   []ItemsRevertidos
+}
+
+type ItemsReversionData struct {
+	Cantidad      string
+	Descripcion   string
+	Identificador string
+	Monto         string
 }
 
 type PagoData struct {
@@ -57,6 +67,7 @@ type PagoData struct {
 	IdPago            string
 	Estado            string
 }
+
 type IntentoData struct {
 	IdIntento     string
 	IdTransaccion string
@@ -98,9 +109,10 @@ func (r *reversion) buildTitle() {
 		r.m.Row(12, func() {
 
 			r.m.Col(6, func() {
-				r.m.Text("Informe de Reversiones de Pagos", props.Text{
+				r.m.Text("Reversiones de Pagos", props.Text{
 					Top:   1,
 					Align: consts.Center,
+					Size:  12,
 				})
 			})
 
@@ -125,9 +137,9 @@ func (r *reversion) buildTitle() {
 	}) // Fin de RegisterHEader
 }
 
-func (r *reversion) buildHeadingsReversiones(data []HeaderReversionData) {
+func (r *reversion) buildHeadingsReversiones(data []ReversionData) {
 
-	for _, header := range data {
+	for _, dato := range data {
 
 		// fila 1 Datos on Top
 		r.m.Row(7, func() {
@@ -137,7 +149,7 @@ func (r *reversion) buildHeadingsReversiones(data []HeaderReversionData) {
 			// col1 Ref Externa
 			r.m.Col(4, func() {
 
-				r.m.Text("Referencia Externa: "+header.Pago.ReferenciaExterna, props.Text{
+				r.m.Text("Referencia Externa: "+dato.Pago.ReferenciaExterna, props.Text{
 					// Top:    3,
 					Left:   2,
 					Style:  consts.Bold,
@@ -149,7 +161,7 @@ func (r *reversion) buildHeadingsReversiones(data []HeaderReversionData) {
 
 			// col2 Medio de pago
 			r.m.Col(4, func() {
-				r.m.Text("Medio de Pago: "+header.Pago.MedioPago, props.Text{
+				r.m.Text("Medio de Pago: "+dato.Pago.MedioPago, props.Text{
 					// Top:    3,
 					Left:   2,
 					Style:  consts.Bold,
@@ -161,7 +173,7 @@ func (r *reversion) buildHeadingsReversiones(data []HeaderReversionData) {
 
 			// col3 Monto
 			r.m.Col(4, func() {
-				r.m.Text("Monto: "+header.Pago.Monto, props.Text{
+				r.m.Text("Monto: "+dato.Pago.Monto, props.Text{
 					// Top:    3,
 					Left:   2,
 					Style:  consts.Bold,
@@ -187,7 +199,7 @@ func (r *reversion) buildHeadingsReversiones(data []HeaderReversionData) {
 					Color:  getDarkGrayColor(),
 				})
 
-				r.m.Text("Id: "+header.Pago.IdPago, props.Text{
+				r.m.Text("Id: "+dato.Pago.IdPago, props.Text{
 					Top:    7,
 					Left:   2,
 					Family: consts.Courier,
@@ -195,7 +207,7 @@ func (r *reversion) buildHeadingsReversiones(data []HeaderReversionData) {
 					Color:  getDarkGrayColor(),
 				})
 
-				r.m.Text("Estado: "+header.Pago.Estado, props.Text{
+				r.m.Text("Estado: "+dato.Pago.Estado, props.Text{
 					Top:  14,
 					Left: 2,
 
@@ -217,7 +229,7 @@ func (r *reversion) buildHeadingsReversiones(data []HeaderReversionData) {
 					Color:  getDarkGrayColor(),
 				})
 
-				r.m.Text("Id Intento: "+header.Intento.IdIntento, props.Text{
+				r.m.Text("Id Intento: "+dato.Intento.IdIntento, props.Text{
 					Top:    7,
 					Left:   2,
 					Family: consts.Courier,
@@ -225,21 +237,21 @@ func (r *reversion) buildHeadingsReversiones(data []HeaderReversionData) {
 					Color:  getDarkGrayColor(),
 				})
 
-				r.m.Text("Transaccion: "+header.Intento.IdTransaccion, props.Text{
+				r.m.Text("Transaccion: "+dato.Intento.IdTransaccion, props.Text{
 					Top:    14,
 					Left:   2,
 					Family: consts.Courier,
 					Size:   10,
 					Color:  getDarkGrayColor(),
 				})
-				r.m.Text("Fecha: "+header.Intento.FechaPago, props.Text{
+				r.m.Text("Fecha: "+dato.Intento.FechaPago, props.Text{
 					Left:   2,
 					Top:    21,
 					Family: consts.Courier,
 					Size:   10,
 					Color:  getDarkGrayColor(),
 				})
-				r.m.Text("Importe: "+header.Intento.Importe, props.Text{
+				r.m.Text("Importe: "+dato.Intento.Importe, props.Text{
 					Left:   2,
 					Top:    28,
 					Family: consts.Courier,
@@ -250,11 +262,65 @@ func (r *reversion) buildHeadingsReversiones(data []HeaderReversionData) {
 			})
 		})
 
+		r.m.SetBorder(false)
+
+		// Mostrar los items de cada pago revertido
+		buildBodyItems(r.m, dato.Items)
+
 		// Agregar una nueva pagina para separar las reversiones
 		r.m.AddPage()
 
 	} // fin de for range
 
+}
+
+func buildBodyItems(m pdf.Maroto, items []ItemsRevertidos) {
+	header, contents := getMediumContent(items)
+	m.Line(1)
+	m.Row(7, func() {
+		m.Col(12, func() {
+			m.Text("Items del Pago", props.Text{Align: consts.Left, Left: 2, Size: 10, Top: 2})
+		})
+	})
+	m.Line(1)
+
+	m.TableList(header, contents, props.TableList{
+		ContentProp: props.TableListContent{
+			Family:    consts.Courier,
+			Style:     consts.Italic,
+			GridSizes: []uint{2, 4, 4, 2},
+		},
+		HeaderProp: props.TableListContent{
+			GridSizes: []uint{2, 4, 4, 2},
+			Family:    consts.Courier,
+			Style:     consts.BoldItalic,
+			Color:     color.Color{100, 0, 0},
+		},
+		Line: true,
+		LineProp: props.Line{
+			Color: color.Color{
+				Red:   128,
+				Green: 221,
+				Blue:  205,
+			},
+			Style: consts.Dashed,
+		},
+	})
+}
+
+func (r *reversion) buildFooter() {
+	r.m.SetFirstPageNb(1)
+	r.m.RegisterFooter(func() {
+		r.m.Row(5, func() {
+			r.m.Col(12, func() {
+				r.m.Text(strconv.Itoa(r.m.GetCurrentPage()), props.Text{
+					Align: consts.Right,
+					Size:  8,
+					Top:   10,
+				})
+			})
+		})
+	})
 }
 
 func getFakeReversiones() []Reversiones {
@@ -274,152 +340,61 @@ func getFakeReversiones() []Reversiones {
 	json.Unmarshal([]byte(byteValue), &rever)
 	return rever
 }
-func getSmallContent() ([]string, [][]string) {
-	header := []string{"Origin", "Destiny", "Cost"}
+
+func getMediumContent(items []ItemsRevertidos) ([]string, [][]string) {
+	header := []string{"Cantidad", "Descripcion", "Identificador", "Monto"}
 
 	contents := [][]string{}
-	contents = append(contents, []string{"São Paulo", "Rio de Janeiro", "R$ 20,00"})
-	contents = append(contents, []string{"São Carlos", "Petrópolis", "R$ 25,00"})
-	contents = append(contents, []string{"São José do Vale do Rio Preto", "Osasco", "R$ 20,00"})
-	contents = append(contents, []string{"Osasco", "São Paulo", "R$ 5,00"})
-	contents = append(contents, []string{"Congonhas", "Fortaleza", "R$ 100,00"})
-	contents = append(contents, []string{"Natal", "Santo André", "R$ 200,00"})
-	contents = append(contents, []string{"Rio Grande do Norte", "Sorocaba", "R$ 44,00"})
-	contents = append(contents, []string{"Campinas", "Recife", "R$ 56,00"})
-	contents = append(contents, []string{"São Vicente", "Juiz de Fora", "R$ 35,00"})
-	contents = append(contents, []string{"Taubaté", "Rio de Janeiro", "R$ 82,00"})
-	contents = append(contents, []string{"Suzano", "Petrópolis", "R$ 62,00"})
-	contents = append(contents, []string{"Jundiaí", "Florianópolis", "R$ 21,00"})
-	contents = append(contents, []string{"Natal", "Jundiaí", "R$ 12,00"})
-	contents = append(contents, []string{"Niterói", "Itapevi", "R$ 21,00"})
-	contents = append(contents, []string{"São Paulo", "Rio de Janeiro", "R$ 31,00"})
-	contents = append(contents, []string{"São Carlos", "Petrópolis", "R$ 42,00"})
-	contents = append(contents, []string{"Florianópolis", "Osasco", "R$ 19,00"})
-	contents = append(contents, []string{"Rio Grande do Norte", "Sorocaba", "R$ 42,00"})
-	contents = append(contents, []string{"Campinas", "Recife", "R$ 58,00"})
-	contents = append(contents, []string{"Florianópolis", "Osasco", "R$ 21,00"})
-	contents = append(contents, []string{"Campinas", "Recife", "R$ 56,00"})
-	contents = append(contents, []string{"São Vicente", "Juiz de Fora", "R$ 35,00"})
-	contents = append(contents, []string{"Taubaté", "Rio de Janeiro", "R$ 82,00"})
-	contents = append(contents, []string{"Suzano", "Petrópolis", "R$ 62,00"})
 
-	return header, contents
-}
-
-func getMediumContent() ([]string, [][]string) {
-	header := []string{"Origin", "Destiny", "Cost per Hour"}
-
-	contents := [][]string{}
-	contents = append(contents, []string{"São José do Vale do Rio Preto", "Osasco", "R$ 12,00"})
-	contents = append(contents, []string{"Niterói", "Itapevi", "R$ 2,10"})
-	contents = append(contents, []string{"São Paulo", "Rio de Janeiro", "R$ 3,10"})
-	contents = append(contents, []string{"São Carlos", "Petrópolis", "R$ 4,20"})
-	contents = append(contents, []string{"Florianópolis", "Osasco", "R$ 1,90"})
-	contents = append(contents, []string{"Osasco", "São Paulo", "R$ 0,70"})
-	contents = append(contents, []string{"Congonhas", "Fortaleza", "R$ 11,30"})
-	contents = append(contents, []string{"Natal", "Santo André", "R$ 19,80"})
-	contents = append(contents, []string{"Rio Grande do Norte", "Sorocaba", "R$ 4,20"})
-	contents = append(contents, []string{"Campinas", "Recife", "R$ 5,80"})
-	contents = append(contents, []string{"São Vicente", "Juiz de Fora", "R$ 3,90"})
-	contents = append(contents, []string{"Jundiaí", "Florianópolis", "R$ 2,30"})
-	contents = append(contents, []string{"Natal", "Jundiaí", "R$ 1,10"})
-	contents = append(contents, []string{"Natal", "Santo André", "R$ 19,80"})
-	contents = append(contents, []string{"Rio Grande do Norte", "Sorocaba", "R$ 4,20"})
-	contents = append(contents, []string{"Campinas", "Recife", "R$ 5,80"})
-	contents = append(contents, []string{"São Vicente", "Juiz de Fora", "R$ 3,90"})
-	contents = append(contents, []string{"Taubaté", "Rio de Janeiro", "R$ 7,70"})
-	contents = append(contents, []string{"Suzano", "Petrópolis", "R$ 6,40"})
-	contents = append(contents, []string{"Jundiaí", "Florianópolis", "R$ 2,00"})
-	contents = append(contents, []string{"Florianópolis", "Osasco", "R$ 1,90"})
-	contents = append(contents, []string{"Osasco", "São Paulo", "R$ 0,70"})
-	contents = append(contents, []string{"Congonhas", "São José do Vale do Rio Preto", "R$ 11,30"})
-	contents = append(contents, []string{"Natal", "Santo André", "R$ 19,80"})
-	contents = append(contents, []string{"Rio Grande do Norte", "Sorocaba", "R$ 4,20"})
+	for _, item := range items {
+		contents = append(contents, []string{item.Cantidad, item.Descripcion, item.Identificador, item.Monto})
+	}
 
 	return header, contents
 }
 
 func main() {
-	begin := time.Now()
+
 	reversionPdf := pdf.NewMaroto(consts.Portrait, consts.A4)
 	reversionPdf.SetPageMargins(10, 15, 10)
 
 	var rev reversion
 	rev.m = reversionPdf
 
-	// headerSmall, smallContent := getSmallContent()
-	headerMedium, mediumContent := getMediumContent()
-
-	// rev.m.SetAliasNbPages("algo")
-	// rev.m.SetFirstPageNb(1)
-
 	rev.buildTitle()
 
 	//Obtener reversiones fake. reversiones es un slice de reportedtos.Reversiones
 	reversiones := getFakeReversiones()
-	var sliceHeaderData []HeaderReversionData
+	var sliceHeaderData []ReversionData
 
 	// Parsear manualmente las reversiones a la data struct para presentar el pdf
 	for _, oneRever := range reversiones {
-		var header_data HeaderReversionData
+		var data ReversionData
 		// Datos del pago en header
-		header_data.Pago.ReferenciaExterna = oneRever.PagoRevertido.ReferenciaExterna
-		header_data.Pago.MedioPago = oneRever.MedioPago
-		header_data.Pago.Monto = oneRever.Monto
-		header_data.Pago.IdPago = oneRever.PagoRevertido.IdPago
-		header_data.Pago.Estado = oneRever.PagoRevertido.PagoEstado
+		data.Pago.ReferenciaExterna = oneRever.PagoRevertido.ReferenciaExterna
+		data.Pago.MedioPago = oneRever.MedioPago
+		data.Pago.Monto = oneRever.Monto
+		data.Pago.IdPago = oneRever.PagoRevertido.IdPago
+		data.Pago.Estado = oneRever.PagoRevertido.PagoEstado
 		// Datos del intento de pago en herader
-		header_data.Intento.IdIntento = oneRever.PagoRevertido.IntentoPago.IdIntentoPago
-		header_data.Intento.IdTransaccion = oneRever.PagoRevertido.IntentoPago.IdTransaccion
-		header_data.Intento.FechaPago = oneRever.PagoRevertido.IntentoPago.FechaPago
-		header_data.Intento.Importe = oneRever.PagoRevertido.IntentoPago.ImportePagado
-
-		sliceHeaderData = append(sliceHeaderData, header_data)
+		data.Intento.IdIntento = oneRever.PagoRevertido.IntentoPago.IdIntentoPago
+		data.Intento.IdTransaccion = oneRever.PagoRevertido.IntentoPago.IdTransaccion
+		fecha := strings.Split(oneRever.PagoRevertido.IntentoPago.FechaPago, " ")
+		data.Intento.FechaPago = fecha[0]
+		data.Intento.Importe = oneRever.PagoRevertido.IntentoPago.ImportePagado
+		data.Items = oneRever.PagoRevertido.Items
+		sliceHeaderData = append(sliceHeaderData, data)
 	}
 
+	// registrar el footer
+	rev.buildFooter()
+	// Las cabeceras de cada pago revertidos y los items de cada pago
 	rev.buildHeadingsReversiones(sliceHeaderData)
 
-	rev.m.AddPage()
-
-	rev.m.Row(15, func() {
-		rev.m.Col(12, func() {
-			rev.m.Text(fmt.Sprintf("Medium Packages / %du.", len(mediumContent)), props.Text{
-				Top:   8,
-				Style: consts.Bold,
-			})
-		})
-	})
-
-	rev.m.TableList(headerMedium, mediumContent, props.TableList{
-		ContentProp: props.TableListContent{
-			Family:    consts.Courier,
-			Style:     consts.Italic,
-			GridSizes: []uint{5, 5, 2},
-		},
-		HeaderProp: props.TableListContent{
-			GridSizes: []uint{5, 5, 2},
-			Family:    consts.Courier,
-			Style:     consts.BoldItalic,
-			Color:     color.Color{100, 0, 0},
-		},
-		Align: consts.Center,
-		Line:  true,
-		LineProp: props.Line{
-			Color: color.Color{
-				Red:   128,
-				Green: 221,
-				Blue:  205,
-			},
-			Style: consts.Dashed,
-		},
-	})
-
-	err := rev.m.OutputFileAndClose("pdfs/sample1.pdf")
+	err := rev.m.OutputFileAndClose("pdfs/reversiones.pdf")
 	if err != nil {
 		fmt.Println("Could not save PDF:", err)
 		os.Exit(1)
 	}
 
-	end := time.Now()
-	fmt.Println(end.Sub(begin))
 }
